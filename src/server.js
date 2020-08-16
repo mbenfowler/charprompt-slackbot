4ce02d6746c84818c0cb25f497ed909aebfc739a
mbfowler <matthew.fowler@procore.com>
2020-08-16 14:31:04 -0700

    respond async

--- src/server.js
+++ src/server.js
@@ -5,39 +5,49 @@ const serverless = require('serverless-http');
 const bodyParser = require('body-parser')
 const app = express();
 const randomChar = require('./randomCharacter');
+const axios = require('axios')
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.json({
-    "text": randomChar()
-  });
-});
-router.post('/', (req, res) =>  {
-    console.log({query: req.query})
     res.json({
-    "delete_original": true,
-    "replace_original": true,
-    "response_type": "in_channel",
-    "text": randomChar(),
-    "blocks": [
-		{
-			"type": "context",
-			"elements": [
-				{
-					"type": "mrkdwn",
-					"text": `:crystal_ball: ${req.body.user_name} summoned me!`
-				}
-			]
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": randomChar(),
-			}
-		}
-	]
-  })});
+        "text": randomChar()
+    });
+});
+router.post('/', (req, res) => {
+    console.log({ body: req.body })
+    res.json({
+        "delete_original": true,
+    })
+    axios.post(req.body.request_url, {
+        "response_type": "in_channel",
+        "text": randomChar(),
+        "blocks": [
+            {
+                "type": "context",
+                "elements": [
+                    {
+                        "type": "mrkdwn",
+                        "text": `:crystal_ball: ${req.body.user_name} summoned me!`
+                    }
+                ]
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": randomChar(),
+                }
+            }
+        ]
+    })
+    .then((res) => {
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(res)
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+});
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
