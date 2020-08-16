ecd509b3d084dd1e8e5e23b4325fd922ecdf1b7d
mbfowler <matthew.fowler@procore.com>
2020-08-16 12:30:18 -0700

    use body data

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ router.post('/', (req, res) =>  {
 			"elements": [
 				{
 					"type": "mrkdwn",
-					"text": `:crystal_ball: ${req.query.user_name} summoned me!`
+					"text": `:crystal_ball: ${req.body.user_name} summoned me!`
 				}
 			]
 		},
