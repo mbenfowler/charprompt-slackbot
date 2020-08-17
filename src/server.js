229b8025f380933524671ace11ff1051a3aa8656
mbfowler <matthew.fowler@procore.com>
2020-08-17 15:30:45 -0700

    formatting

--- src/server.js
+++ src/server.js
@@ -8,34 +8,35 @@ const randomChar = require('./randomCharacter');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.json({
-    "text": randomChar()
-  });
-});
-router.post('/', (req, res) =>  {
-    console.log({body: req.body})
     res.json({
-    "response_type": "in_channel",
-    "text": randomChar(),
-    "blocks": [
-		{
-			"type": "context",
-			"elements": [
-				{
-					"type": "mrkdwn",
-					"text": `:crystal_ball: ${req.body.user_name} summoned me!`
-				}
-			]
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": randomChar(),
-			}
-		}
-	]
-  })});
+        "text": randomChar()
+    });
+});
+router.post('/', (req, res) => {
+    console.log({ body: req.body })
+    res.json({
+        "response_type": "in_channel",
+        "text": randomChar(),
+        "blocks": [
+            {
+                "type": "context",
+                "elements": [
+                    {
+                        "type": "mrkdwn",
+                        "text": `:crystal_ball: ${req.body.user_name} summoned me!`
+                    }
+                ]
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": randomChar(),
+                }
+            }
+        ]
+    })
+});
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
